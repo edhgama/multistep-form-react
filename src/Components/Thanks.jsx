b2bbb2c94e4e9526594c0a17613e5a3bb5db9d0f
Edheyberg Gama <edhgama@gmail.com>
2023-08-16 15:27:12 -0400

    thanks-view

--- src/Components/Thanks.jsx
+++ src/Components/Thanks.jsx
@@ -1,6 +1,28 @@
+import { 
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsEmojiFrownFill,
+  BsEmojiHeartEyesFill
+} from 'react-icons/bs'
+
+import "./thanks.css";
+
+
 export default function Thanks() {
 
   return (
-    <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta Pouco...</h2>
+      <p>A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra</p>
+      <p>Para concluir a sua avaliação clique no botão Enviar.</p>
+      <h3>Aqui está o resumo da sua avaliação</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto</span>
+      </p>
+      <p className='review-data'>
+        <span>Comentário:</span>
+      </p>
+    
+    </div>
   )
 }
