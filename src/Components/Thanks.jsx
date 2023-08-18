c6d25b8e6462dd9198203b414e69cd739fb4e6eb
Edheyberg Gama <edhgama@gmail.com>
2023-08-18 15:54:23 -0400

    add vite deploy workflow

--- src/Components/Thanks.jsx
+++ src/Components/Thanks.jsx
@@ -5,24 +5,30 @@ import {
   BsEmojiHeartEyesFill
 } from 'react-icons/bs'
 
-import "./thanks.css";
+import "./Thanks.css";
 
+const emojiData = {
+  unsatisfied: <BsEmojiFrownFill/>,
+  neutral:<BsFillEmojiNeutralFill/>,
+  sastified:<BsFillEmojiSmileFill/>,
+  very_satisfied:<BsEmojiHeartEyesFill/>
+}
 
 export default function Thanks({data}) {
 
   return (
     <div className="thanks-container">
-      <h2>Falta Pouco...</h2>
+      <h3>Falta Pouco...</h3>
       <p>A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra</p>
       <p>Para concluir a sua avaliação clique no botão Enviar.</p>
-      <h3>Aqui está o resumo da sua avaliação</h3>
+      <h3>Aqui está o resumo da sua avaliação {data.name}</h3>
       <p className="review-data">
-        <span>Satisfação com o produto</span>
+        <span>Satisfação com o produto:</span> {emojiData[data.review]}
       </p>
       <p className='review-data'>
-        <span>Comentário:</span>
+        <span>Comentário:</span> {data.comment}
       </p>
-    
+      
     </div>
   )
 }
